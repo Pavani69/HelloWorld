94c2be0262249facf20e17e181807307f98f758d
Pavani69 <41293008+Pavani69@users.noreply.github.com>
2018-07-18 12:44:46 +0530

    Commited the code to the java
    Commited the code to the java

---
+++ TestDemoJenkins.java
@@ -0,0 +1,13 @@
+package TestPDemo;
+
+import org.testng.annotations.Test;
+
+public class TestDemoJenkins {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Welcome to Jenkins");
+	}
+
+}
